a521e5b7c93e4800ddff34e43d86352c263f5751
Aaron Kling <webgeek1234@gmail.com>
2020-11-15 21:16:54 -0600

    Remove all makefiles, init scripts, and vintf snippets
    These have been moved to device trees to better facilitate vendor
    script generation.

--- common/bcm_firmware/bcm4356/device-bcm.mk
+++
@@ -1,4 +0,0 @@
-BCM_FW_SRC_FILE_STA := fw_4356a3_prod.bin
-
-PRODUCT_COPY_FILES += \
-    vendor/nvidia/common/bcm_firmware/bcm4356/$(BCM_FW_SRC_FILE_STA):$(TARGET_COPY_OUT_VENDOR)/firmware/fw_bcmdhd.bin
