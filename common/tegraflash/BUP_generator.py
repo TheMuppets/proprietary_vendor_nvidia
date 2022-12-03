b5295f4c07fad1395ca2978b70839ac8aefde01b
Aaron Kling <webgeek1234@gmail.com>
2022-12-03 11:10:28 -0600

    common: bmp blob should use nvblob v2 magic

--- common/tegraflash/BUP_generator.py
+++ common/tegraflash/BUP_generator.py
@@ -237,7 +237,7 @@ class payload():
         return version
 
     def __init__(self, args):
-        self.magic = bup_magic
+        self.magic = "NVIDIA__BLOB__V2" if args.blob_type == "bmp" else bup_magic
         self.version = 0x00020000 if args.blob_type == "bmp" else self.gen_version()
         self.blob_size_pos = struct.calcsize('=16sI')
         self.header_packing = '=16sIIIIII'
